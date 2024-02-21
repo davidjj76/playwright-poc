c551c4bfc1faf71af2990d8517c2346163dd24ac
davidjj76 <davidjj76@gmail.com>
2024-02-21 22:05:12 +0100

    Add console.log

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has welcome message', async ({ page }) => {
   await page.goto('/');
 
+  console.log('CI is', process.env.CI);
+
   await expect(page.getByText('Welcome to Vite from Playwright')).toBeVisible();
 });
