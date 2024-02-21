aa02418036bb08c2ee13b7fad0ada4ea05f8a247
davidjj76 <davidjj76@gmail.com>
2024-02-21 22:08:25 +0100

    Point to localhost

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has welcome message', async ({ page }) => {
-  await page.goto('/');
-
   console.log('CI is', process.env.CI);
-
+  await page.goto('/');
   await expect(page.getByText('Welcome to Vite from Playwright')).toBeVisible();
 });
