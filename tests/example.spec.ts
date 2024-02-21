26e1f3f98f3f5a7a2bd97dbbb9387ba7aac5ebe7
davidjj76 <davidjj76@gmail.com>
2024-02-21 22:50:33 +0100

    Snapshots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has welcome message', async ({ page }) => {
+test('should have welcome message', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByText('Welcome to Vite from Playwright')).toBeVisible();
+});
+
+test('should have snapshot', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveScreenshot('homepage.png', { fullPage: true });
 });
