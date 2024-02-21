2bbbc170cd4aeecb19c7b21afc2b6d82a0b2da18
davidjj76 <davidjj76@gmail.com>
2024-02-21 22:10:30 +0100

    Remove console.log

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('has welcome message', async ({ page }) => {
-  console.log('CI is', process.env.CI);
   await page.goto('/');
   await expect(page.getByText('Welcome to Vite from Playwright')).toBeVisible();
 });
